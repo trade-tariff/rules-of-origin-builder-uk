40275223ba8e4a12915df86b26c2f42c9bf0cc84
Matt Lavis <matthew.lavis@gmail.com>
2022-12-02 11:17:09 +0000

    Further fix to chapter links

--- classes/rule.py
+++ classes/rule.py
@@ -246,10 +246,11 @@ class Rule(object):
 
         self.rule_string = re.sub("chapter ([0-9]{1,2}) to ([0-9]{1,2})([., ])", "chapter \\1 to chapter \\2\\3", self.rule_string)
 
+        self.rule_string = re.sub("([Cc]hapter)(s*) ([0-9])([ ,;.])", "\\1\\2 0\\3\\4", self.rule_string)  # Links in markdown
         self.rule_string = re.sub("([Cc]hapter)(s*) ([0-9]{1,2}) and ([0-9]{1,2})", "[\\1 \\3](/chapters/\\3) and chapter \\4", self.rule_string)  # Links in markdown
         self.rule_string = re.sub("([Cc]hapter)(s*) ([0-9]{1,2}) or ([0-9]{1,2})", "[\\1 \\3](/chapters/\\3) or chapter \\4", self.rule_string)  # Links in markdown
         self.rule_string = re.sub("([Cc]hapter)(s*) ([0-9])([ ,;.])", "[\\1\\2  \\3](/chapters/0\\3)\\4", self.rule_string)  # Links in markdown
-        self.rule_string = re.sub("([Cc]hapter)(s*) ([0-9]{1,2})([ ,;.])", "[\\1\\2  \\3](/chapters/\\3)\\4", self.rule_string)  # Links in markdown
+        self.rule_string = re.sub("([Cc]hapter)(s*) ([0-9]{1,2})([ ,;.])", "[\\1\\2 \\3](/chapters/\\3)\\4", self.rule_string)  # Links in markdown
 
         self.rule_string = re.sub(" +", " ", self.rule_string)
         if self.heading == "Chapter 6":
